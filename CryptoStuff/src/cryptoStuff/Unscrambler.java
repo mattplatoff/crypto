2c051d42fb26cd82e66eb49651a8faf1a7cf0ffe
53141 <53141@HSS-WIN7V110.westwindsor.k12.nj.us>
2014-06-04 19:29:59 -0400

    ayyy yooo

--- CryptoStuff/src/cryptoStuff/Unscrambler.java
+++ CryptoStuff/src/cryptoStuff/Unscrambler.java
@@ -1,9 +1,39 @@
 package cryptoStuff;
 
-public class Unscrambler {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
+public class Unscrambler {
+String text;
+	public Unscrambler(String path) throws FileNotFoundException{
+		Scanner scan=new Scanner(new File(path));
+		while (scan.hasNext())
+		text+=scan.nextLine();
+	}
+
+	public void print(){
+	String printer = null;
+	Scanner scan=new Scanner(text);
+	for (int n=0;n<text.length();n++){
+		printer+=scan.next();
+		if (n%10==0){
+			printer+="/n";
+		}
+	}
+	System.out.println(printer);
 	
 	}
+public void freq(String text){
+	int [] freq=new int[26];
+	
+	
+	
+	
+	
+	
+}
+
+
 
 }
